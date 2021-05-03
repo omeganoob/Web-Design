0fee07dfd3aef78a517061083f5f94a48dd47308
omeganoob <nguyenchung666644136@gmail.com>
2021-05-03 22:16:23 +0700

    add front_mountains animation

--- parallax/script.js
+++ parallax/script.js
@@ -12,6 +12,7 @@ window.addEventListener('scroll', ()=>{
   stars.style.top = value * -1 + 'px';
   moon.style.top = value + 'px';
   mountains_behind.style.top = value * 0.5 + 'px';
+  mountains_front.style.top = value * 0.5 + 'px';
   text.style.marginRight = value * 4 + 'px';
   text.style.marginTop = value * 1.5 + 'px';
   btn.style.marginTop = value * 1.5 + 'px';
